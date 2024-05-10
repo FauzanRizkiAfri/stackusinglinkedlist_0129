0874671b4ae0ce3d7bb49b77b4eeb684230ceef3
FauzanRizkiAfri <163797425+FauzanRizkiAfri@users.noreply.github.com>
2024-05-10 10:13:35 +0700

    2

--- stackusinglinkedlist_0129/stackusinglinkedlist_0129.cpp
+++ stackusinglinkedlist_0129/stackusinglinkedlist_0129.cpp
@@ -4,11 +4,18 @@ using namespace std;
 
 // Node class representing a single node in the linked list
 class Node {
- public:
-			int data;
-			Node * next;
-			
-	    	Node() {
-		    	next = NULL;
-			}
-	}; 
+public:
+	int data;
+	Node* next;
+
+	Node() {
+		next = NULL;
+	}
+};
+
+// Stack class
+class Stack {
+private:
+	Node* top; // Pointer to the top node of the stack
+
+
